9127535aa61fa2a2d209e273443f772b11dd29db
Abdul Samad <samadbinabdulla123@gmail.com>
2023-07-24 20:18:58 -0400

    img opt2

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js', // Your main entry point
@@ -6,16 +7,17 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'bundle.js', // Output file name
   },
+  mode: 'production', // Enable production mode for tree shaking and minification
   module: {
     rules: [
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|otf)$/,
-            use: ['file-loader'],
-          },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
       {
         test: /\.jsx?$/, // Transpile JS/JSX files
         exclude: /node_modules/,
@@ -27,5 +29,8 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'], // Allow importing JS and JSX files without specifying the extension
+  },
+  optimization: {
+    minimizer: [new TerserPlugin()], // Enable minification
   },
 };
